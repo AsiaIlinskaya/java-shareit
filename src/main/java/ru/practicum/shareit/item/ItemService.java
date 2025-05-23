cb4b88a368789252ed0a7c92a2925a2f0db097da
AsiaIlinskaya <iassiriya@yandex.ru>
2025-05-23 14:51:09 +0300

    Add files via upload

--- src/main/java/ru/practicum/shareit/item/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/ItemService.java
@@ -1,5 +1,6 @@
 package ru.practicum.shareit.item;
 
+import ru.practicum.shareit.comment.dto.CommentDto;
 import ru.practicum.shareit.item.dto.ItemDto;
 
 import java.util.List;
@@ -13,6 +14,8 @@ public interface ItemService {
     ItemDto saveItem(long userId, ItemDto itemDto);
 
     List<ItemDto> findItemsByOwner(long userId);
+
+    CommentDto addComment(long userId, long itemId, String text);
 
     List<ItemDto> searchItems(String searchText);
 
