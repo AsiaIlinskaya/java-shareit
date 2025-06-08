7e7faa46a1153f22cdb491db96ac13d6792de58a
AsiaIlinskaya <iassiriya@yandex.ru>
2025-06-08 18:00:15 +0300

    Add files via upload

---
+++ server/src/main/java/ru/practicum/shareit/comment/model/Comment.java
@@ -0,0 +1,35 @@
+package ru.practicum.shareit.comment.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.model.User;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "comments")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Текст комментария не может быть пустым")
+    private String text;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id")
+    private User author;
+
+    @NotNull
+    @Column(name = "created_at")
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
