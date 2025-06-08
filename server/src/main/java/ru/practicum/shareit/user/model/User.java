ab75129601e43293719c2da2c9c20fd5d8f3b145
AsiaIlinskaya <iassiriya@yandex.ru>
2025-06-08 18:15:37 +0300

    Add files via upload

---
+++ server/src/main/java/ru/practicum/shareit/user/model/User.java
@@ -0,0 +1,31 @@
+package ru.practicum.shareit.user.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.practicum.shareit.intf.Create;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(groups = Create.class, message = "Имя не может быть пустым")
+    private String name;
+
+    @NotBlank(groups = Create.class, message = "Электронная почта не может быть пустым")
+    @Email(groups = Create.class, message = "электронная почта не может быть пустой и должна содержать символ @")
+    @Column(name = "email", unique = true)
+    private String email;
+}
